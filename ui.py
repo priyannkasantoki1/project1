4d93f276c0abde52fc0a212720a2533db1493179
priyannkasantoki <128717591+priyannkasantoki1@users.noreply.github.com>
2025-02-28 14:22:36 +0530

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,11 @@
+def handler(event, context):
+    with open('index.html', 'r') as file:
+        html_content = file.read()
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/html'
+        },
+        'body': html_content
+    }
+
